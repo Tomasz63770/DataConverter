ec6012232ed36d040817466bb7da60e6f087c9db
Tomasz <tomasz63770@gmail.com>
2026-06-28 11:54:15 +0200

    Task1 - argument parsing

--- main.py
+++ main.py
@@ -2,17 +2,15 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Program do konwersji plików JSON, YAML i XML"
-    )
+    parser = argparse.ArgumentParser(description="Konwerter JSON / YAML / XML")
 
     parser.add_argument("input_file", help="Plik wejściowy")
     parser.add_argument("output_file", help="Plik wyjściowy")
 
     args = parser.parse_args()
 
-    print("Plik wejściowy:", args.input_file)
-    print("Plik wyjściowy:", args.output_file)
+    print("INPUT:", args.input_file)
+    print("OUTPUT:", args.output_file)
 
 
 if __name__ == "__main__":
